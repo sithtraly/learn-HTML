be13f3018f7643a332e6fd96bb0acb5ab651cb76
Sith Traly <sithtraly147@gmail.com>
2024-01-16 13:23:08 +0700

    change icon when hide/show password

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 function togglePassword() {
   const txtPassword = document.getElementById('password')
+  const eyeOn = document.getElementById('eye-on')
+  const eyeOff = document.getElementById('eye-off')
   if (txtPassword.type == 'password') {
     txtPassword.type = 'text'
+    eyeOn.classList.add('none')
+    eyeOff.classList.remove('none')
   } else {
     txtPassword.type = 'password'
+    eyeOn.classList.remove('none')
+    eyeOff.classList.add('none')
   }
 }
