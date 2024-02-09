28d010d72ea587383b409b5d1b841457c501620d
Traly Sith <sithtraly147@gmail.com>
2024-02-09 13:59:17 +0700

    Game over, sound

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -8,27 +8,33 @@ const gridSize = width / totalGrid
 const cv = document.getElementById('canvas')
 const ctx = cv.getContext('2d')
 let direction = 'down'
-let fps = 2
+let fps = 3
 let scores = 0
-const lbScore = new Text(ctx, getX(0) / 2, getY(1) / 2, 'Scores: '+ scores)
+const lbScore = new Text(ctx, getX(0) / 2, getY(1) / 2)
 lbScore.setAlignment('left')
+lbScore.setFont('24px ProtestStrike')
+lbScore.draw()
 
 const eyeSize = 5
 const eyeSpaceFromCenter = 8
-const snakeArray = []
-for (let i = 0; i < 3; i++) {
-  const snake = new Rectangle(ctx, getX(0), getY(i), gridSize, gridSize, 'white')
-  snake.col = 0
-  snake.row = i
-  snakeArray.push(snake)
+let snakeArray = []
+function initSnake(snakeSize = 3) {
+  snakeArray = []
+  for (let i = 0; i < snakeSize; i++) {
+    const snake = new Rectangle(ctx, getX(0), getY(i), gridSize, gridSize, 'white')
+    snake.col = 0
+    snake.row = i
+    snakeArray.push(snake)
+  }
 }
+initSnake()
 const snakeEyes = [
   new Circle(ctx, getX(snakeArray[0].col - eyeSpaceFromCenter), getY(snakeArray[0].row) + 5, eyeSize, 'green'),
   new Circle(ctx, getX(snakeArray[0].col + eyeSpaceFromCenter), getY(snakeArray[0].row) + 5, eyeSize, 'green')
 ]
 
 let food
-let positioned = { col: [], row: [] }
+let isGameOver = false
 
 // set game width and height
 cv.width = width
@@ -102,6 +108,32 @@ function generateFood() {
   return isOccupied ? generateFood() : { col, row }
 }
 
+function drawGameOver() {
+  const lbGameOver = new Text(ctx, width / 2, hegiht * 0.25, 'Game Over')
+  lbGameOver.setFont('56px ProtestStrike')
+  lbGameOver.draw()
+  const lbScores = new Text(ctx, width / 2, hegiht * 0.5, 'Scores: ' + scores)
+  lbScores.setFont('36px ProtestStrike')
+  lbScores.draw()
+  const lbRestart = new Text(ctx, width / 2, hegiht * 0.75, 'Press `r` to restart')
+  lbRestart.setFont('36px ProtestStrike')
+  lbRestart.setColor('pink')
+  lbRestart.draw()
+}
+
+function drawYouWon() {
+  const lbCongrate = new Text(ctx, width / 2, hegiht * 0.25, 'Congratulation')
+  lbCongrate.setFont('48px ProtestStrike')
+  lbCongrate.draw()
+  const lbYouWon = new Text(ctx, width / 2, hegiht * 0.5, 'You Won!!!')
+  lbYouWon.setFont('56px ProtestStrike')
+  lbYouWon.draw()
+  const lbRestart = new Text(ctx, width / 2, hegiht * 0.75, 'Press `r` to restart')
+  lbRestart.setFont('36px ProtestStrike')
+  lbRestart.setColor('pink')
+  lbRestart.draw()
+}
+
 function getX(col = 0) {
   return col * gridSize + gridSize / 2
 }
@@ -117,11 +149,23 @@ function getPos(col, row) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const sounds = {
+    eat: new Audio('/assets/sounds/touch.mp3'),
+    die: new Audio('/assets/sounds/die.mp3')
+  }
   document.addEventListener('keydown', (e) => {
     if (['a', 'ArrowLeft'].includes(e.key) && direction != 'right') direction = 'left'
     else if (['d', 'ArrowRight'].includes(e.key) && direction != 'left') direction = 'right'
     else if (['s', 'ArrowDown'].includes(e.key) && direction != 'up') direction = 'down'
     else if (['w', 'ArrowUp'].includes(e.key) && direction != 'down') direction = 'up'
+    else if (e.key == 'r') {
+      isGameOver = false
+      initSnake()
+      scores = 0
+      food.setX(getX(5))
+      food.setY(getY(5))
+      direction = 'down'
+    }
   })
   drawGrid()
   drawSnack()
@@ -129,41 +173,45 @@ document.addEventListener('DOMContentLoaded', () => {
   lbScore.draw()
 
   setInterval(() => {
-    // if (snakeArray.length >= totalGrid * totalGrid) {
     ctx.clearRect(0, 0, width, hegiht)
-    for (let [i, snake] of snakeArray.entries()) {
-      if (i < snakeArray.length - 1) {
-        snake.col = snakeArray[i + 1].col
-        snake.row = snakeArray[i + 1].row
-      } else if (i >= snakeArray.length - 1) {
-        if (direction == 'up') snake.row -= 1
-        else if (direction == 'down') snake.row += 1
-        else if (direction == 'left') snake.col -= 1
-        else if (direction == 'right') snake.col += 1
-        if (snake.row < 0) snake.row = totalGrid - 1
-        else if (snake.row >= totalGrid) snake.row = 0
-        else if (snake.col < 0) snake.col = totalGrid - 1
-        else if (snake.col >= totalGrid) snake.col = 0
+    if (snakeArray.length < totalGrid * totalGrid && !isGameOver) {
+      for (let [i, snake] of snakeArray.entries()) {
+        if (i < snakeArray.length - 1) {
+          snake.col = snakeArray[i + 1].col
+          snake.row = snakeArray[i + 1].row
+        } else if (i >= snakeArray.length - 1) {
+          if (direction == 'up') snake.row -= 1
+          else if (direction == 'down') snake.row += 1
+          else if (direction == 'left') snake.col -= 1
+          else if (direction == 'right') snake.col += 1
+          if (snake.row < 0) snake.row = totalGrid - 1
+          else if (snake.row >= totalGrid) snake.row = 0
+          else if (snake.col < 0) snake.col = totalGrid - 1
+          else if (snake.col >= totalGrid) snake.col = 0
+        }
       }
-    }
-    drawGrid()
-    drawSnack()
-    const snakeHead = snakeArray[snakeArray.length - 1]
-    if (snakeHead.x == food.x && snakeHead.y == food.y) {
-      positioned = { col: [], row: [] }
-      for (let snake of snakeArray) {
-        positioned.col.push(snake.col)
-        positioned.row.push(snake.row)
+      drawGrid()
+      drawSnack()
+      const snakeHead = snakeArray[snakeArray.length - 1]
+      const tmpSnakeArray = Array.from(snakeArray)
+      tmpSnakeArray.pop()
+      isGameOver = tmpSnakeArray.some((snake) => snake.col == snakeHead.col && snake.row == snakeHead.row)
+      if (isGameOver) sounds.die.play()
+      if (snakeHead.x == food.x && snakeHead.y == food.y) {
+        sounds.eat.play()
+        const newPos = generateFood()
+        food.setX(getX(newPos.col))
+        food.setY(getY(newPos.row))
+        const snake = new Rectangle(ctx, snakeArray[0].col, snakeArray[0].row, gridSize, gridSize, 'white')
+        snakeArray.unshift(snake)
+        scores++
       }
-      const newPos = generateFood()
-      food.setX(getX(newPos.col))
-      food.setY(getY(newPos.row))
-      const snake = new Rectangle(ctx, snakeArray[0].col, snakeArray[0].row, gridSize, gridSize, 'white')
-      snakeArray.unshift(snake)
-      scores++
+      drawFood()
+      lbScore.setText('Scores: ' + scores)
+    } else if (isGameOver) {
+      drawGameOver()
+    } else {
+      drawYouWon()
     }
-    drawFood()
-    lbScore.setText('Scores: ' + scores)
-    // }
   }, 1000 / fps)
 })
